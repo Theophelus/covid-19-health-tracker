13b14cd061c5c3a0777b1dcb5abd397c332a1e0e
Theophelus <aneletom10@gmail.com>
2020-05-18 09:06:15 +0200

    adding json file and images in the resource folder

--- src/main/java/com/philus/covid19healthtracker/service/QuestionService.java
+++ src/main/java/com/philus/covid19healthtracker/service/QuestionService.java
@@ -1,4 +1,16 @@
 package com.philus.covid19healthtracker.service;
 
+import com.philus.covid19healthtracker.models.Answers;
+import com.philus.covid19healthtracker.models.Question;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class QuestionService {
+
+
+    //Define an arrayList to sto List of Questions
+    List<Question> questions = Arrays.asList(new Question(0,"","", new ArrayList<>()));
+
 }
